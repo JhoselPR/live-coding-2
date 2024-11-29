63511170c864ae2a07a4c275e762619b9292fe17
JhoselPR <112789135+JhoselPR@users.noreply.github.com>
2024-11-29 11:04:13 -0600

    Live-coding 2

--- starter-files/script.js
+++ starter-files/script.js
@@ -11,3 +11,34 @@ const API_ENDPOINT = 'https://yesno.wtf/api';
  * 5. Optional: add loading/error states
  *
  */
+
+const fetchAnswer = async () => {
+    try {
+        const response = await fetch(API_ENDPOINT);
+        const parsedResponse = await response.json();
+        return parsedResponse.answer;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const answerContainer = document.getElementById("answer");
+const btnTrigger = document.getElementById("button");
+
+
+btnTrigger.addEventListener("click", async () => {
+    const userInput = document.getElementById("input").value;
+    if (userInput.trim() !== "") {
+        const answer = await fetchAnswer();
+        answerContainer.textContent = answer;
+        setTimeout(() => {
+            answerContainer.textContent = "";
+          }, 5000);
+    }else {
+        alert("Please enter a question");
+    }
+})
+
+
+
+
